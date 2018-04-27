79b66275cdb04f5459638eb3bc48bd900849ec39
SavageCore <talk@savagecore.eu>
2018-04-27 17:56:09 +0100

    Simplify logic by converting all arguments to a single enum

--- index.js
+++ index.js
@@ -7,65 +7,21 @@ module.exports.templateTags = [{
 	description: 'Generate a random UK bank sort code',
 	args: [
 		{
-			displayName: 'Barclays',
-			description: 'Barclays',
-			type: 'boolean',
-			defaultValue: true
-		},
-		{
-			displayName: 'HSBC',
-			description: 'HSBC',
-			type: 'boolean',
-			defaultValue: false
-		},
-		{
-			displayName: 'Lloyds',
-			description: 'Lloyds',
-			type: 'boolean',
-			defaultValue: false
-		},
-		{
-			displayName: 'Santander UK',
-			description: 'Santander UK',
-			type: 'boolean',
-			defaultValue: false
-		},
-		{
-			displayName: 'The Co-operative Bank',
-			description: 'The Co-operative Bank',
-			type: 'boolean',
-			defaultValue: false
-		},
-		{
-			displayName: 'The Royal Bank of Scotland',
-			description: 'The Royal Bank of Scotland',
-			type: 'boolean',
-			defaultValue: false
+			displayName: 'Bank',
+			type: 'enum',
+			options: [
+				{displayName: 'Barclays', value: 'barclays'},
+				{displayName: 'HSBC', value: 'hsbc'},
+				{displayName: 'Lloyds', value: 'lloyds'},
+				{displayName: 'Santander UK', value: 'santander'},
+				{displayName: 'The Co-operative Bank', value: 'coop'},
+				{displayName: 'The Royal Bank of Scotland', value: 'rbs'}
+			]
 		}
 	],
-	async run(context, barclays, hsbc, lloyds, santander, coop, rbs) { // eslint-disable-line max-params
-		const options = [];
-		if (barclays) {
-			options.push('barclays');
-		}
-		if (hsbc) {
-			options.push('hsbc');
-		}
-		if (lloyds) {
-			options.push('lloyds');
-		}
-		if (santander) {
-			options.push('santander');
-		}
-		if (coop) {
-			options.push('coop');
-		}
-		if (rbs) {
-			options.push('rbs');
-		}
-		const option = options[Math.floor(Math.random() * (options.length - 0))];
-    // Based on https://en.wikipedia.org/wiki/Sort_code#London_clearings
-		switch (option) {
+	async run(context, bank = 'barclays') {
+		// Based on https://en.wikipedia.org/wiki/Sort_code#London_clearings
+		switch (bank) {
 			case 'barclays':
 				return `${randomInt(20, 29)}-${zeroFill(2, randomInt(0, 99))}-${zeroFill(2, randomInt(0, 99))}`;
 			case 'hsbc':
@@ -83,7 +39,7 @@ module.exports.templateTags = [{
 			case 'rbs':
 				return `${randomInt(15, 18)}-${zeroFill(2, randomInt(0, 99))}-${zeroFill(2, randomInt(0, 99))}`;
 			default:
-				return 0;
+				return 'Error - No bank selected';
 		}
 	}
 }];
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import {templateTags} from '.';
 const inRange = require('in-range');
 
 test('Barclays', async t => {
-	const ret = await templateTags[0].run('', true, false, false, false, false, false);
+	const ret = await templateTags[0].run('', 'barclays');
 	const retArr = ret.split('-');
 	t.true(inRange(Number(retArr[0]), 20, 29));
 	t.true(inRange(Number(retArr[1]), 0, 99));
@@ -12,7 +12,7 @@ test('Barclays', async t => {
 });
 
 test('HSBC', async t => {
-	const ret = await templateTags[0].run('', false, true, false, false, false, false);
+	const ret = await templateTags[0].run('', 'hsbc');
 	const retArr = ret.split('-');
 	t.true(inRange(Number(retArr[0]), 40, 49));
 	t.true(inRange(Number(retArr[1]), 0, 99));
@@ -20,7 +20,7 @@ test('HSBC', async t => {
 });
 
 test('Lloyds', async t => {
-	const ret = await templateTags[0].run('', false, false, true, false, false, false);
+	const ret = await templateTags[0].run('', 'lloyds');
 	const retArr = ret.split('-');
 	t.true(inRange(Number(retArr[0]), 30, 39));
 	t.true(inRange(Number(retArr[1]), 0, 99));
@@ -28,7 +28,7 @@ test('Lloyds', async t => {
 });
 
 test('Santander', async t => {
-	const ret = await templateTags[0].run('', false, false, false, true, false, false);
+	const ret = await templateTags[0].run('', 'santander');
 	const retArr = ret.split('-');
 	t.is(retArr[0], '09');
 	t.true(inRange(Number(retArr[1]), 0, 19));
@@ -36,7 +36,7 @@ test('Santander', async t => {
 });
 
 test('Co-op', async t => {
-	const ret = await templateTags[0].run('', false, false, false, false, true, false);
+	const ret = await templateTags[0].run('', 'coop');
 	const retArr = ret.split('-');
 	t.is(retArr[0], '08');
 	t.true(inRange(Number(retArr[1]), 60, 61) || inRange(Number(retArr[1]), 90, 99));
@@ -44,7 +44,7 @@ test('Co-op', async t => {
 });
 
 test('RBS', async t => {
-	const ret = await templateTags[0].run('', false, false, false, false, false, true);
+	const ret = await templateTags[0].run('', 'rbs');
 	const retArr = ret.split('-');
 	t.true(inRange(Number(retArr[0]), 15, 18));
 	t.true(inRange(Number(retArr[1]), 0, 99));
@@ -52,6 +52,6 @@ test('RBS', async t => {
 });
 
 test('Default', async t => {
-	const ret = await templateTags[0].run('', false, false, false, false, false, false);
-	t.is(ret, 0);
+	const ret = await templateTags[0].run('', '');
+	t.is(ret, 'Error - No bank selected');
 });
