6d3061d78f77009d13c3dbac1864f78ffa815e53
SavageCore <talk@savagecore.eu>
2018-04-27 20:54:30 +0100

    Switch from `in-range` to `ow` in tests
    s

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import ow from 'ow';
 import {templateTags} from '.';
 
 const inRange = require('in-range');
@@ -6,49 +7,64 @@ const inRange = require('in-range');
 test('Barclays', async t => {
 	const ret = await templateTags[0].run('', 'barclays');
 	const retArr = ret.split('-');
-	t.true(inRange(Number(retArr[0]), 20, 29));
-	t.true(inRange(Number(retArr[1]), 0, 99));
-	t.true(inRange(Number(retArr[2]), 0, 99));
+	t.notThrows(() => ow(Number(retArr[0]), ow.number.inRange(20, 29)));
+	t.notThrows(() => ow(Number(retArr[1]), ow.number.inRange(0, 99)));
+	t.notThrows(() => ow(Number(retArr[2]), ow.number.inRange(0, 99)));
 });
 
 test('HSBC', async t => {
 	const ret = await templateTags[0].run('', 'hsbc');
 	const retArr = ret.split('-');
-	t.true(inRange(Number(retArr[0]), 40, 49));
-	t.true(inRange(Number(retArr[1]), 0, 99));
-	t.true(inRange(Number(retArr[2]), 0, 99));
+	t.notThrows(() => ow(Number(retArr[0]), ow.number.inRange(40, 49)));
+	t.notThrows(() => ow(Number(retArr[1]), ow.number.inRange(0, 99)));
+	t.notThrows(() => ow(Number(retArr[2]), ow.number.inRange(0, 99)));
 });
 
 test('Lloyds', async t => {
 	const ret = await templateTags[0].run('', 'lloyds');
 	const retArr = ret.split('-');
-	t.true(inRange(Number(retArr[0]), 30, 39));
-	t.true(inRange(Number(retArr[1]), 0, 99));
-	t.true(inRange(Number(retArr[2]), 0, 99));
+	t.notThrows(() => ow(Number(retArr[0]), ow.number.inRange(30, 39)));
+	t.notThrows(() => ow(Number(retArr[1]), ow.number.inRange(0, 99)));
+	t.notThrows(() => ow(Number(retArr[2]), ow.number.inRange(0, 99)));
 });
 
 test('Santander', async t => {
 	const ret = await templateTags[0].run('', 'santander');
 	const retArr = ret.split('-');
 	t.is(retArr[0], '09');
-	t.true(inRange(Number(retArr[1]), 0, 19));
-	t.true(inRange(Number(retArr[2]), 0, 99));
+	t.notThrows(() => ow(Number(retArr[1]), ow.number.inRange(0, 19)));
+	t.notThrows(() => ow(Number(retArr[2]), ow.number.inRange(0, 99)));
 });
 
 test('Co-op', async t => {
 	const ret = await templateTags[0].run('', 'coop');
 	const retArr = ret.split('-');
 	t.is(retArr[0], '08');
-	t.true(inRange(Number(retArr[1]), 60, 61) || inRange(Number(retArr[1]), 90, 99));
-	t.true(inRange(Number(retArr[2]), 0, 99));
+	t.notThrows(() => {
+		let errs = 0;
+		try {
+			ow(Number(retArr[1]), ow.number.inRange(60, 61));
+		} catch (err) {
+			errs += 1;
+		}
+		try {
+			ow(Number(retArr[1]), ow.number.inRange(90, 99));
+		} catch (err) {
+			errs += 1;
+		}
+		if (errs === 2) {
+			throw (new Error(`Expected ${retArr[1]} to be in range [60..61] or [90..99]`));
+		}
+	});
+	t.notThrows(() => ow(Number(retArr[2]), ow.number.inRange(0, 99)));
 });
 
 test('RBS', async t => {
 	const ret = await templateTags[0].run('', 'rbs');
 	const retArr = ret.split('-');
-	t.true(inRange(Number(retArr[0]), 15, 18));
-	t.true(inRange(Number(retArr[1]), 0, 99));
-	t.true(inRange(Number(retArr[2]), 0, 99));
+	t.notThrows(() => ow(Number(retArr[0]), ow.number.inRange(15, 18)));
+	t.notThrows(() => ow(Number(retArr[1]), ow.number.inRange(0, 99)));
+	t.notThrows(() => ow(Number(retArr[2]), ow.number.inRange(0, 99)));
 });
 
 test('Default', async t => {
